56d91e87848709e93362b99e82020f76f88aa960
manisha_soni <manisha.mangoit@gmail.com>
2023-09-29 20:14:17 +0530

    changes

--- models/questionnames.js
+++ models/questionnames.js
@@ -1,24 +1,24 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class questionNames extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      questionNames.hasMany(models.questions, { foreignKey: 'qname_id', as: 'questions' });
+    class questionNames extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            questionNames.hasMany(models.questions, { foreignKey: 'qname_id', as: 'questions' });
+        }
     }
-  }
-  questionNames.init({
-    question_name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'questionNames',
-  });
-  return questionNames;
+    questionNames.init({
+        question_name: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'questionNames',
+    });
+    return questionNames;
 };
